ca6aded5ef209ac8fc1f53d08495eb08ab4edc05
曦 <gnail1022@gmail.com>
2013-11-07 00:11:01 +0800

    Update TableCreation_Project.sql

--- DataMgmt/scripts/TableCreation_Project.sql
+++ DataMgmt/scripts/TableCreation_Project.sql
@@ -1,15 +1,14 @@
-USE [QA_Automation_Test_POC]
+USE [Automation]
 GO
 
-/****** Object:  Table [QA_Automation_Test].[Project]    Script Date: 03/25/2013 14:32:26 ******/
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[QA_Automation_Test].[Project]') AND type in (N'U'))
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Automation].[Project]') AND type in (N'U'))
 DROP TABLE [QA_Automation_Test].[Project]
 GO
 
-USE [QA_Automation_Test_POC]
+USE [Automation]
 GO
 
-/****** Object:  Table [QA_Automation_Test].[Project]    Script Date: 03/25/2013 14:32:26 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -19,7 +18,7 @@ GO
 SET ANSI_PADDING ON
 GO
 
-CREATE TABLE [QA_Automation_Test].[Project](
+CREATE TABLE [Automation].[Project](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[ProjectName] [varchar](50) NOT NULL,
 	[Platform] [varchar](20) NOT NULL,
